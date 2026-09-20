aad98439e1e19354c1c580866ab16ab04bea1974
trestbp-jpg <trest.bp@gmail.com>
2026-09-20 10:55:35 +0200

    Add veris-call/app/src/main/java/ru/veris/call/AppConfig.kt

---
+++ veris-call/app/src/main/java/ru/veris/call/AppConfig.kt
@@ -0,0 +1,5 @@
+package ru.veris.call
+object AppConfig {
+    const val ENDPOINT = "https://veris39.ru/wp-json/veris/v1/call"
+    const val APP_TOKEN = "LzwtV6uBAlS1O63wiqkFTsAw_lTIGxlNpQPiZMBpjZ9aEXn-GZbdicKmvCB5gKr0"
+}
